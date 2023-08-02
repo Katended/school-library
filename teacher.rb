175e3058b5b24e46549a3abcd741d699f8a3419a
David Katende <katendedavidjoga@gmail.com>
2023-08-02 14:52:30 +0300

    Fix paramters

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, specialization, parent_permission = Trrue, name = 'Unknown')
+  def initialize(age, specialization, parent_permission = True, name = 'Unknown')
     super(age, parent_permission, name)
     @specialization = specialization
   end
