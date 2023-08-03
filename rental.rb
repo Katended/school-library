4e5b5659e8beeddfbaebe91c950f4068b947e92c
David Katende <katendedavidjoga@gmail.com>
2023-08-03 18:41:04 +0300

    Add UI

--- rental.rb
+++
@@ -1,9 +0,0 @@
-class Rental
-  attr_accessor :date
-
-  def initialize(date)
-    @date = date
-    book.add_rental(self)
-    person.add_rental(self)
-  end
-end
--- src/person.rb
+++ src/person.rb
@@ -3,16 +3,24 @@ require_relative 'capitalizedecorator'
 require_relative 'trimmerdecorator'
 
 class Person < Nameable
-  attr_reader :id
-  attr_accessor :name, :age, :rentals
+  attr_reader :id, :rentals
+  attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
-    super()
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
@@ -21,18 +29,8 @@ class Person < Nameable
     @age >= 18
   end
 
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
   def generate_id
     rand(1..1000)
-  end
-
-  def correct_name
-    puts @name
   end
 
   def add_rental(rental)
