79adb3da122a01995b09b264c9896c82817b6857
David Katende <katendedavidjoga@gmail.com>
2023-08-08 00:14:08 +0300

    Add list class

--- main.rb
+++ main.rb
@@ -1,2 +1,153 @@
-require_relative 'app/menu'
-Menu.new
+require_relative 'app/app'
+require_relative 'src/person'
+require_relative 'src/teacher'
+require_relative 'src/student'
+require_relative 'src/book'
+require_relative 'src/rental'
+
+class Main
+  def initialize   
+    puts
+    puts 'WELCOME TO THE SCHOOL LIBRARY APP!'
+     @app = App.new(self)
+    show_menu
+  end
+
+  def show_menu
+    puts
+    puts 'Please choose an option by entering a number:'
+    puts '1 -  List all books'
+    puts '2 -  List all people'
+    puts '3 -  Create a person'
+    puts '4 -  Create a book'
+    puts '5 -  Create a rental'
+    puts '6 -  List all rentals for a given person id'
+    puts '7 -  Exit'
+    puts
+
+    user_choice = gets.chomp
+
+    select_option(user_choice)
+
+  end
+
+  def select_option(user_choice)
+    options = {
+      '1' => :list_all_books,
+      '2' => :list_all_people,
+      '3' => :create_person,
+      '4' => :create_book,
+      '5' => :create_rental,
+      '6' => :list_all_rentals,
+      '7' => :exit
+    }
+
+    method = options[user_choice]
+    if method.nil?
+      puts 'Invalid option, please try again!'
+      puts
+      show_menu
+    else
+      self.send(method)
+    end
+  end
+
+  def list_all_people
+    @app.list_all_people
+    show_menu
+  end
+
+  def list_all_books
+    @app.list_all_books
+    show_menu
+  end
+
+  def list_all_rentals
+    @app.list_all_rentals
+    show_menu
+  end
+
+  def create_person
+    puts 'Do you want to create a student(1) or a teacher(2)? [Input the number]'
+    person_role = gets.chomp
+
+    person_options = {
+      '1' => :create_student,
+      '2' => :create_teache  
+    }
+
+    method = person_options[person_role]
+
+    if method.nil?
+      puts 'Please add a valid input!'
+      puts
+      show_menu    
+    end
+
+    case person_role
+    when '1'
+
+      print 'Age: '
+      age = gets.chomp.to_i
+      print 'Name: '
+      name = gets.chomp
+      print 'Has parent permission? [Y/N]: '
+      parent_permission = gets.chomp.downcase == 'y'
+      print 'Classroom: '
+      classroom = gets.chomp.to_i
+      @app.add_people_list(Student.new(age, classroom, name, parent_permission: parent_permission))
+      puts
+      puts 'Person created successfully'
+
+    when '2'
+
+      print 'Age: '
+      age = gets.chomp
+      print 'Name: '
+      name = gets.chomp
+      print 'Specialization: '
+      specialization = gets.chomp
+      @app.add_people_list(Teacher.new(age, specialization, name, parent_permission: true))    
+      puts
+      puts 'Person created successfully'
+    else
+      puts 'Please add a valid input!'
+    end    
+     show_menu
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    @app.add_book_list(Book.new(title, author))
+    puts 'Book created successfully'
+    show_menu
+  end
+  
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @app.books_list.each_with_index { |book, index| puts "#{index}) Title: '#{book.title}', Author: #{book.author}" }
+    book_num = gets.chomp.to_i
+    puts
+    puts 'Select a person from the following list by number (not id)'
+    @app.people_list.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person_num = gets.chomp.to_i
+    print 'Date: '
+    date = gets.chomp
+    @app.add_rental_list(Rental.new(date, @app.books_list[book_num], @app.people_list[person_num]))
+    puts 'Rental created successfully'
+    show_menu
+  end
+
+  def exit_app
+    puts 'Thank you! Have a great day!'
+    exit(0)
+  end
+end
+
+
+Main.new
